36a5e6e3c2c7a44aa821051a68b244bffe6fd8d7
filippofini <filippo.fini@mail.polimi.it>
2024-10-13 10:02:48 +0200

    Added completed challenge. Reversing is completed.

---
+++ reverse/leaked_license/test.py
@@ -0,0 +1,12 @@
+s = "f3ed47e2 6e4de24a414981945c7da2db1ac93d5"
+s2 = "726cfc2d 26c6defe db065621 99f5c7d0 da4f4930"
+
+ADD = 0x5555555552b2
+
+"""
+set *0x7fffffffd9e8=0x726cfc2d
+set *0x7fffffffd9e8=0x26c6defe
+set *0x7fffffffd9e8=0xdb065621
+set *0x7fffffffd9e8=0x99f5c7d0
+set *0x7fffffffd9e8=0xda4f4930
+"""
